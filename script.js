601fcbf5a72c802f6bb9f5b9b0424c445203a62b
Gene Edwards <eedwards@su.edu>
2019-12-13 13:48:58 -0500

    Updated five day forecast display

--- script.js
+++ script.js
@@ -38,18 +38,18 @@ function displayCity() {
     }).then(function (response) {
 
         //Converting the temperature to fahrenheit
-        var temp1 = Math.round(((response.list[0].main.temp - 273.15) * 9 / 5) + 32);
-        var temp2 = Math.round(((response.list[6].main.temp - 273.15) * 9 / 5) + 32);
-        var temp3 = Math.round(((response.list[15].main.temp - 273.15) * 9 / 5) + 32);
-        var temp4 = Math.round(((response.list[23].main.temp - 273.15) * 9 / 5) + 32);
-        var temp5 = Math.round(((response.list[32].main.temp - 273.15) * 9 / 5) + 32);
+        var temp1 = Math.round(((response.list[1].main.temp - 273.15) * 9 / 5) + 32);
+        var temp2 = Math.round(((response.list[9].main.temp - 273.15) * 9 / 5) + 32);
+        var temp3 = Math.round(((response.list[17].main.temp - 273.15) * 9 / 5) + 32);
+        var temp4 = Math.round(((response.list[25].main.temp - 273.15) * 9 / 5) + 32);
+        var temp5 = Math.round(((response.list[33].main.temp - 273.15) * 9 / 5) + 32);
   
         //Putting the response dates in variables
-        var date1 = new Date(response.list[0].dt_txt);
-        var date2 = new Date(response.list[6].dt_txt);
-        var date3 = new Date(response.list[15].dt_txt);
-        var date4 = new Date(response.list[23].dt_txt);
-        var date5 = new Date(response.list[32].dt_txt);
+        var date1 = new Date(response.list[1].dt_txt);
+        var date2 = new Date(response.list[9].dt_txt);
+        var date3 = new Date(response.list[17].dt_txt);
+        var date4 = new Date(response.list[25].dt_txt);
+        var date5 = new Date(response.list[33].dt_txt);
 
         //Displaying five day dates using getMonth/getDate/getFullYear methods
         $("#dateDay1").text(`${date1.getMonth() + 1}/${date1.getDate()}/${date1.getFullYear()}`);
@@ -66,18 +66,18 @@ function displayCity() {
         $("#tempDay5").html(`Temp: ${temp5} <span>&#8457;</span>`);
 
         //Displaying five day icons
-        $("#iconDay1").attr("src", `http://openweathermap.org/img/wn/${response.list[0].weather[0].icon}@2x.png`);
-        $("#iconDay2").attr("src", `http://openweathermap.org/img/wn/${response.list[6].weather[0].icon}@2x.png`);
-        $("#iconDay3").attr("src", `http://openweathermap.org/img/wn/${response.list[15].weather[0].icon}@2x.png`);
-        $("#iconDay4").attr("src", `http://openweathermap.org/img/wn/${response.list[23].weather[0].icon}@2x.png`);
-        $("#iconDay5").attr("src", `http://openweathermap.org/img/wn/${response.list[32].weather[0].icon}@2x.png`);
+        $("#iconDay1").attr("src", `http://openweathermap.org/img/wn/${response.list[1].weather[0].icon}@2x.png`);
+        $("#iconDay2").attr("src", `http://openweathermap.org/img/wn/${response.list[9].weather[0].icon}@2x.png`);
+        $("#iconDay3").attr("src", `http://openweathermap.org/img/wn/${response.list[17].weather[0].icon}@2x.png`);
+        $("#iconDay4").attr("src", `http://openweathermap.org/img/wn/${response.list[25].weather[0].icon}@2x.png`);
+        $("#iconDay5").attr("src", `http://openweathermap.org/img/wn/${response.list[33].weather[0].icon}@2x.png`);
 
         //Displaying five day humidity
-        $("#humDay1").html(`Humidity: ${response.list[0].main.humidity}%`);
-        $("#humDay2").html(`Humidity: ${response.list[6].main.humidity}%`);
-        $("#humDay3").html(`Humidity: ${response.list[15].main.humidity}%`);
-        $("#humDay4").html(`Humidity: ${response.list[23].main.humidity}%`);
-        $("#humDay5").html(`Humidity: ${response.list[32].main.humidity}%`);
+        $("#humDay1").html(`Humidity: ${response.list[1].main.humidity}%`);
+        $("#humDay2").html(`Humidity: ${response.list[9].main.humidity}%`);
+        $("#humDay3").html(`Humidity: ${response.list[17].main.humidity}%`);
+        $("#humDay4").html(`Humidity: ${response.list[25].main.humidity}%`);
+        $("#humDay5").html(`Humidity: ${response.list[33].main.humidity}%`);
 
     });
 
